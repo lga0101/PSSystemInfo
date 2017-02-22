d9c447ecd2e1ca9b53100494880204d2554e9287
mskccdanielm1 <mskccdanielm1@users.noreply.github.com>
2017-02-22 16:25:59 -0500

    Adding Private Functions
    Adding an export Excel private function to test auto CSV -> XLSX with
    conditional formatting (i.e. highlight failed machine rows in red).

---
+++ Private/Export-ExcelWrapper.ps1
@@ -0,0 +1,8 @@
+﻿### WIP Excel formatting ###
+
+function Export-ServerReportXLSX { 
+
+Import-Module ImportExcel
+$report = "severstatus.csv"
+Get-Content $report | Export-Excel -Show -Now -ConditionalText (New-ConditionalText -ConditionalType ContainsText "RED" -ConditionalTextColor red)
+}
