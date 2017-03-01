4b55e87c15ca4a771a5b69113a7d51f0cc43f24c
mskccdanielm1 <mskccdanielm1@users.noreply.github.com>
2017-03-01 09:15:39 -0500

    Extension Check - Return
    Return true/false when checking if the specified path is a directory or
    file.

--- Private/Get-Extension.ps1
+++ Private/Get-Extension.ps1
@@ -1,16 +1,20 @@
 function Get-Extension {
-    Write-Host $ExportPath
+
+Param(
+    
+    [Parameter()]
+    [string]
+    $ExportPath
+
+    )
+
     [string]$Extension = ($ExportPath[-4..-1])
     $Extension = $Extension.Replace(" ","")
-    Write-Host "Extension Is $Extension"
-    if  ($Extension -eq "xlsx" -or $Extension -eq ".xls" -or $Extension -eq ".csv" -or $Extension -eq ".txt") {
-        $ExtensionCheck = "True"
-        Write-Host $Extension
-        Write-Host $ExtensionCheck
-    }
+    
+    if ($Extension -eq "xlsx" -or $Extension -eq ".xls" -or $Extension -eq ".csv" -or $Extension -eq ".txt") {
+        Return "True"
+        }
     else { 
-        Write-Host $Extension 
-        $ExtensionCheck = "False"
-        Write-Host $ExtensionCheck
-}
+        Return "False"
+        }
 }
